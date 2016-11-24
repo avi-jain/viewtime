b59a0dfeb48ea8d7ee752fe4eea9dae45d617fed
janga <janga1997@gmail.com>
2016-11-24 22:10:29 +0530

    Support for elements partially in viewport

--- viewport.js
+++ viewport.js
@@ -3,8 +3,9 @@
  * Copyright (c) 2016
  */
 
-//polluting the global namespace
-var counter = []
+//polluting the global namespace 
+var counter = [];
+var fraction = 1;
 
 // window.addEventListener('load', initialize);
 
@@ -21,48 +22,60 @@ if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and 
   visibilityChange = "webkitvisibilitychange";
 }
 
-function initialize(list){
+function initialize(list, percent){
 
-	if(arguments.length == 0){
+  if(percent != undefined){
+    fraction = 0.01*percent;
+  }
+
+  if(list.length == 0){
     list = document.querySelectorAll('[id]');
     //console.log(list)
     //here list is an HTML collection and many browsers don't support forEach over 
     // NodeLists. Hence typecasting
     Array.from(list).forEach(function(element) 
-      {
+    {
           //Add all elements to an array of objects
           counter.push({id:element.id, time:0});
           
-      });
+        });
   }
 
   //Now list is an array and not a HTML Collection but since it's a passed argument
   // it's an array-like object :/
   else{
     Array.from(list).forEach(function(id) 
-      {
-          counter.push({id:id, time:0});   
-      });
+    {
+      counter.push({id:id, time:0});   
+    });
   }
   //console.log(list)
-	timerId = setInterval(startTimers.bind(null,list),1000);
+  timerId = setInterval(startTimers.bind(null,list),1000);
 
 }
 
 function checkViewport(el) {
-	//check if element is in viewport.If in viewport increase count
-	// http://www.w3schools.com/js/js_window.asp works for all "modern" browsers
-  //console.log(el);
-	var rect = el.getBoundingClientRect();
+  //check if element is in viewport.If in viewport increase count
+  // http://www.w3schools.com/js/js_window.asp works for all "modern" browsers
+/*  console.log(el);
+*/  var rect = el.getBoundingClientRect();
 
-	//entirely within viewport
-	if (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&     
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+var top = parseInt(rect.top, 10);
+var bottom = parseInt(rect.bottom, 10);
+var right = parseInt(rect.right, 10);
+var left = parseInt(rect.left, 10);
+
+var windowHeight = parseInt((window.innerHeight || document.documentElement.clientHeight), 10);
+var windowWidth = parseInt((window.innerWidth || document.documentElement.clientWidth), 10);
+
+  //entirely within viewport
+  if (
+    top >= 0 &&
+    left >= 0 &&
+    bottom <= windowHeight &&    
+    right <= windowWidth
     ) 
-    {
+  {
       //console.log(counter)
       //Increase viewtime count
       counter.forEach(function(object) 
@@ -72,13 +85,13 @@ function checkViewport(el) {
           object.time = object.time+1;
         }
       });
-  	}
+    }
 
   //bigger than viewport
   //needs better logic?
-	if (rect.height > window.innerHeight || document.documentElement.clientHeight) {
-			if((rect.top) < 0 && (rect.bottom)>document.documentElement.clientHeight){
-				//console.log(el);
+  if (rect.height > windowHeight) {
+    if(top < 0 && bottom > fraction*windowHeight){
+        //console.log(el);
         counter.forEach(function(object) 
         {
           if (object.id == el.id)
@@ -87,21 +100,21 @@ function checkViewport(el) {
           }
         });
       }
-	}	      
+    }       
 
-	// Partially in viewport     
-    
+  // Partially in viewport     
+  
 }
 
 function startTimers(list) {
-		Array.from(list).forEach(function(element) {
+  Array.from(list).forEach(function(element) {
       //If list is the list of all ids created through the script, it will be a HTML collection
       // So, the next line need not be executed.
       if(!(list instanceof NodeList)){
         element = document.getElementById(element);
       }
-	    checkViewport(element);
-		});
+      checkViewport(element);
+    });
 }
 
 if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
@@ -115,13 +128,13 @@ if (typeof document.addEventListener === "undefined" || typeof document[hidden] 
 function pauseTimer(){
   //check if tab is active or not. If inactive stop counters
   if(document.hidden){
-  	clearInterval(timerId);
+    clearInterval(timerId);
   } 
 }
 function resumeTimer(){
   //check if tab is active or not. If active again, resume counters
   if(!(document.hidden)){
-  	timerId = setInterval(startTimers.bind(null,list),1000);
+    timerId = setInterval(startTimers.bind(null,list),1000);
   } 
 }
 
